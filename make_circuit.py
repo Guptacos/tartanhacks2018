a8719d6df694495933df2483b26fd4b0ea39262d
Niko Gupta <Niko@MacBook-Pro-8.local>
2018-02-10 14:40:34 -0500

    successfully (I think) changed make_circuit to function with error messages instead of raising an exception

--- make_circuit.py
+++ make_circuit.py
@@ -5,66 +5,61 @@ colMax = 7
 rowMax = 5
 
 def get_circuit(grid):      #grid is a 2d list
-    output = None
     rowNum = 0
     colNum = colMax
     err = ""
-
+    circuit = None
     while True:     #finds rightmost output gate
         val = grid[rowNum][colNum]
-        if val != None: 
-            output = val
-            break
+        if val != None: break
         rowNum += 1
+        
         if rowNum > rowMax:
             rowNum = 0
             colNum -= 1
-        if colNum < 0: invalid_circuit("No gates found!")
+        if colNum < 0: err = "No gates found!"
+    
     #at this point, rowNum and rowCol are the coordinates of the output val
     #now we have to check that there are no gates in the same row as the output
-    #grid[rowNum][colNum] = None
-    curCol = get_col(grid, colNum)
-    #if not is_empty(curCol): invalid_circuit("Can't have multiple outputs!")
-    for index in range(rowMax + 1):
-        if index == rowNum: continue
-        elif curCol[index] != None: 
-            invalid_circuit("Can't have multiple outputs!")
+    if err == "":
+        curCol = get_col(grid, colNum)
+        for index in range(rowMax + 1):
+            if index == rowNum: continue
+            elif curCol[index] != None: 
+                err = "Can't have multiple outputs!"
         
-
-    print("output gate = ", rowNum, colNum, val)
-    circuit = create_elem(grid, val, rowNum, colNum)
-    #check that every circuit element has been used
-    #for row in grid:
-    #    if not is_empty(row):
-    #        print(row)
-    #        print(circuit)
-    #        invalid_circuit("parts of circuit disconnected")
-    return circuit
+    if err == "":
+        (circuit, err) = create_elem(grid, val, rowNum, colNum)
+        #check that every circuit element has been used
+        #for row in grid:
+        #    if not is_empty(row):
+        #        invalid_circuit("parts of circuit disconnected")
+    return (circuit, err)
 
 def create_elem(grid, val, curRow, curCol):
-    #grid[curRow][curCol] = None
-#    if val == None: raise Exception("Error: tried to create None gate")
+    err = ""
 
-    if val == "A": return CInput("A")
-    elif val == "B": return CInput("B")
-    elif val == "C": return CInput("C")
-    elif val == "D": return CInput("D") #base cases
+    if val == "A": return (CInput("A"), err)
+    elif val == "B": return (CInput("B"), err)
+    elif val == "C": return (CInput("C"), err)
+    elif val == "D": return (CInput("D"), err) #base cases
     
     else:
-        if curCol ==0:
-            print(curRow, curCol, val)
-            invalid_circuit("Can't have gate on left edge of board.")
-        print(curRow, curCol)
-        (in1, in2, row1, row2) = get_closest(grid,curRow,curCol)
-        circ1 = create_elem(grid, in1, row1, curCol-1)
-        circ2 = create_elem(grid, in2, row2, curCol-1)
-        if val == "and": return AndGate(circ1, circ2)
-        elif val == "or": return OrGate(circ1, circ2)
-        elif val == "xor": return XorGate(circ1, circ2)
-        elif val == "not": return NotGate(circ1) #circ1 == circ2 if not gate
-    #if you reach this part of the code, that means you hit an undefined gate
-    print(val)
-    raise Exception("Error: tried to create undefined gate")
+        if curCol == 0:
+            err = "Can't have gate on left edge of board."
+        if err == "":
+            ((in1, in2, row1, row2), err) = get_closest(grid,curRow,curCol)
+        if err == "":
+            (circ1, err) = create_elem(grid, in1, row1, curCol-1)
+        if err == "":
+            (circ2, err) = create_elem(grid, in2, row2, curCol-1)
+        if err == "":
+            if val == "and": return (AndGate(circ1, circ2), err)
+            elif val == "or": return (OrGate(circ1, circ2), err)
+            elif val == "xor": return (XorGate(circ1, circ2), err)
+            elif val == "not": return (NotGate(circ1), err) #circ1 == circ2 
+    #if you reach this part of the code, that means you hit an error. Return err
+    return (None, err)
 
 def is_empty(row):
     for item in row:
@@ -81,25 +76,28 @@ def get_col(grid, col):
     return result
 
 def get_closest(grid, row, col):
-    if col == 0: invalid_circuit("Oops! your gate was too far left.")
-    colPrev = get_col(grid, col-1)
+    err = ""
+    if col == 0: err = "Oops! your gate was too far left."
+    
+    if err == "":
+        colPrev = get_col(grid, col-1)
 
-    if col%2 == 0:      #case where offset of our grid is relevant
-        if ((row == 0 and colPrev[row] == None) or
-           (row == rowMax and colPrev[row-1] == None)): 
-            invalid_circuit("Uh oh! Your gate didn't have an input.")
-        elif row == 0: return (colPrev[row], colPrev[row], row, row)
-        elif row == rowMax: return (colPrev[row-1], colPrev[row-1], row-1,row-1)
-    return check_behind(grid, row, col)
+    if err == "":
+        if col%2 == 0:      #case where offset of our grid is relevant
+            if ((row == 0 and colPrev[row] == None) or
+               (row == rowMax and colPrev[row-1] == None)): 
+                err = "Uh oh! Your gate didn't have an input."
+            elif row == 0: return ((colPrev[row], colPrev[row], row, row), err)
+            elif row == rowMax: return ((colPrev[row-1], colPrev[row-1], row-1,row-1), err)
+    if err == "":
+        return (check_behind(grid, row, col), err)
+    return (None, None, None, None, err)
         
 def check_behind(grid, row, col):
     colPrev = get_col(grid, col-1)
     (val1, row1) = (colPrev[row-1], row-1) if (col%2 == 0) else (colPrev[row], row)
     (val2, row2) = (colPrev[row], row) if (col%2 == 0) else (colPrev[row+1], row+1)
-    print(val1, row1, val2, row2)
+    
     if (val1 != None and val2 != None): return (val1, val2, row1, row2)
     elif (val1 == None): return (val2, val2, row2, row2)
     else: return (val1, val1, row1, row1)
-
-def invalid_circuit(string):
-    raise Exception(string)
