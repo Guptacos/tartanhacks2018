dba0c837af497a06f52d8b5b0359ca11631025e6
Logan Kojiro <logankojiro@Logans-MacBook-Pro.local>
2018-02-10 03:01:43 -0500

    updated imagesHandler to have offset column heights

--- imagesHandler.py
+++ imagesHandler.py
@@ -24,17 +24,22 @@ def getGridImages(img):
     gridWidth = src.size[0] // horiz
     gridHeight = src.size[1] // vert
 
-    for row in range(vert):
-        for col in range(horiz):
+    for col in range(horiz):
+        for row in range(vert):
             print("Row: %d Col: %d"%(row,col))
             left = col * gridWidth
-            upper = row * gridHeight
             right = (col + 1) * gridWidth
-            lower = (row + 1) * gridHeight
+
+            if (col%2 == 0):
+                upper = row * gridHeight
+                lower = (row + 1) * gridHeight
+            else:
+                upper = row * gridHeight + (gridHeight/2)
+                lower = (row + 1) * gridHeight + (gridHeight/2)
+
             temp = src.crop((left,upper,right,lower))
             temp.save("testImg/%d-%d.jpg" %(row,col))
-            print("left: %d right: %d upper: %d Lower: %d"
-                     %(left,right,upper,lower))
+            
 
 
 
