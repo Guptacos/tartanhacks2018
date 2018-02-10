dc670ee91b2be0bee4de94d9914e6d9487230320
Niko Gupta <Niko@MacBook-Pro-8.local>
2018-02-10 03:01:59 -0500

    modified gates.py and digital_circuit.py to make inheritance more useful (i.e. reduced redundancies). Also added functionality to return the truth table and qm minimized equation for any circtuit

--- digital_circuit.py
+++ digital_circuit.py
@@ -1,7 +1,31 @@
+from equation_parse import *
+from ourQM import *
+
 class circuit(object):
     def __init__(self,in1,in2):
         self.in1=in1
         self.in2=in2
+        self.userEq = self.get_user_eq
+        self.truthTable = None
+        self.qm = None
+
+    def __repr__(self):
+        return self.userEq
+
+    def get_table(self):
+        if self.truthTable == None
+            self.truthTable = make_truth_table(self.userEq)
+        return self.truthTable
+
+    def get_qm(self):
+        if self.qm == None:
+            self.qm = qm(self.get_table)
+        return self.qm
+
+    def get_user_eq(self):
+        raise Exception("You forgot to define get_user_eq in a child function 
+                        you goon")
+        return None
 
 class input(object):
     def __init__(self,name):
